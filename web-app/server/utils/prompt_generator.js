b4b942956b0ccbd1f93148390153913ed38409ec
cbgalileo <cbrice@plu.edu>
2026-05-13 13:25:00 -0700

    sidebar update & prompt generator guidelines
    - changed the style of the sidebar as the button look of the elements did not fit the visual style of the rest of the app as well
    - added guidelines (doc comments) for the prompt generator to explain its purpose and the design structure

--- web-app/server/utils/prompt_generator.js
+++ web-app/server/utils/prompt_generator.js
@@ -1,3 +1,28 @@
+/** 
+ * Prompt Generator Guidelines
+ * 
+ * The purpose of the generator is to provide a structured variety of ideas for writers
+ * to inspire creativity without fully detailing a story.
+ * 
+ * Templates should be grammatically flexible with multiple word banks, with decently-high reusability based on the word banks used.
+ * General word banks should be flexible across a wide variety of genres without being too specific towards any one in particular.
+ * Genre-specific word banks should reinforce tone without making prompts overly restrictive.
+ * 
+ * The resulting prompt should be generally open-ended, allowing writers to have full creative
+ * freedom with what they choose to write about.
+*/
+
+/**
+ * Templates should establish a narrative setup or tension, using placeholders that are
+ * interchangable to ensure a wide variety of different combanations for a single template.
+ * 
+ * Placeholders should not have "a", "an", or "the" before them, as that would disrupt the
+ * general flow of the template and lowers reusability. Instead, they should go inside the
+ * word bank itself.
+ * 
+ * Templates should not expect highly-specific outputs for a word bank, and should generally
+ * work with most entries and across any genre.
+ */
 const templates = [
   `{character_adj} {character} discovers {object} {location}, but {condition}.`,
   `{character_adj} {character} receives {object} with a message that reads: "{message}".`,
@@ -11,6 +36,13 @@ const templates = [
   `everyone believes {condition}, but {character_adj} {character} knows the truth.`
 ];
 
+/**
+ * General word banks should work across multiple genres and avoid any overly-specific details. They
+ * should be compatible with any template and encourage many different variations for any singular 
+ * template.
+ * 
+ * Entries should be relatively short to ensure prompts read naturally.
+ */
 const general_word_banks = {
   character: [
     "traveler",
@@ -153,6 +185,16 @@ const general_word_banks = {
   ]
 };
 
+/**
+ * Genre-specific word banks should be compatible with any template, while also reinforcing the tone 
+ * and/or atmosphere of a given genre without making prompts too repetitive or predictable.
+ * 
+ * Add: expands on the general word banks, enhancing variability by providing entries
+ * that are more specific to a given genre.
+ * 
+ * Override: replaces the general word banks that do not suit the given genre well,
+ * providing a different tone or atmosphere compared to the corresponding general word bank.
+ */
 const genre_specific_word_banks = {
   horror: {
     add: {
@@ -194,6 +236,16 @@ const genre_specific_word_banks = {
   }
 };
 
+/**
+ * Challenge prompts provide an alternative structure for prompts, giving only elements
+ * of the story and a challenge rule.
+ * 
+ * Challenge rules are made with the intention of getting writers a bit out of their usual
+ * comfort zones to enhance their writing by providing specific narrative styles, tropes, or
+ * other constraints.
+ * 
+ * Challenge prompts are intentionally more restrictive than standard prompts.
+ */
 const challenge_rules = [
   "don't use dialogue",
   "keep the story under 200 words",
